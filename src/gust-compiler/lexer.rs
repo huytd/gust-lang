3572b8b5812e0e01a838b8367288fb0d16512b07
Huy Tran <kingbazoka@gmail.com>
2022-04-29 00:41:46 -0700

    Lexer rework - part 2 - finished

--- src/gust-compiler/lexer.rs
+++ src/gust-compiler/lexer.rs
@@ -1,15 +1,20 @@
-use std::{iter::Peekable, str::Chars};
+use std::{
+    iter::{Enumerate, Peekable},
+    str::Chars,
+};
 
 use super::token::Token;
 
 pub struct Lexer<'a> {
-    chars: Peekable<Chars<'a>>,
+    chars: Peekable<Enumerate<Chars<'a>>>,
+    source: &'a str,
 }
 
 impl<'a> Lexer<'a> {
     pub fn new(input: &'a str) -> Self {
         Self {
-            chars: input.chars().peekable(),
+            chars: input.chars().enumerate().peekable(),
+            source: input,
         }
     }
 }
@@ -18,11 +23,11 @@ impl<'a> Iterator for Lexer<'a> {
     type Item = Token<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        while let Some(&' ') = self.chars.peek() {
+        while let Some((_, ' ')) = self.chars.peek() {
             self.chars.next();
         }
         // Process Single-char tokens
-        if let Some(c) = self.chars.peek() {
+        if let Some((_, c)) = self.chars.peek() {
             if let Some(token) = match c {
                 '{' => Some(Token::LeftBracket),
                 '}' => Some(Token::RightBracket),
@@ -45,8 +50,8 @@ impl<'a> Iterator for Lexer<'a> {
             }
         }
         // Conditional tokens and others
-        if let Some(c) = self.chars.next() {
-            if let Some(c_next) = self.chars.peek() {
+        if let Some((start, c)) = self.chars.next() {
+            if let Some((_, c_next)) = self.chars.peek() {
                 if let Some(token) = match c {
                     '!' => {
                         if c_next == &'=' {
@@ -78,12 +83,58 @@ impl<'a> Iterator for Lexer<'a> {
                             Some(Token::Less)
                         }
                     }
+                    '&' => {
+                        if c_next == &'&' {
+                            Some(Token::And)
+                        } else {
+                            None
+                        }
+                    }
+                    '|' => {
+                        if c_next == &'|' {
+                            Some(Token::Or)
+                        } else {
+                            None
+                        }
+                    }
                     _ => {
                         if c.is_alphabetic() {
-                            // Identifier
+                            let mut end = start;
+                            while let Some((next_end, c_next)) = self.chars.peek() {
+                                if c_next.is_alphanumeric() || c_next == &'_' {
+                                    end = *next_end;
+                                    self.chars.next();
+                                } else {
+                                    break;
+                                }
+                            }
+                            let word = &self.source[start..=end];
+                            match word {
+                                "if" => return Some(Token::If),
+                                "else" => return Some(Token::Else),
+                                "fn" => return Some(Token::Func),
+                                "for" => return Some(Token::For),
+                                "while" => return Some(Token::While),
+                                "let" => return Some(Token::Let),
+                                "return" => return Some(Token::Return),
+                                "nil" => return Some(Token::Nil),
+                                "true" => return Some(Token::True),
+                                "false" => return Some(Token::False),
+                                "print" => return Some(Token::Print),
+                                _ => return Some(Token::Identifier(word)),
+                            }
                         }
                         if c.is_numeric() {
-                            // Number
+                            let mut end = start;
+                            while let Some((next_end, c_next)) = self.chars.peek() {
+                                if c_next.is_digit(10) || c_next == &'_' || c_next == &'.' {
+                                    end = *next_end;
+                                    self.chars.next();
+                                } else {
+                                    break;
+                                }
+                            }
+                            return Some(Token::Number(&self.source[start..=end]));
                         }
                         None
                     }
